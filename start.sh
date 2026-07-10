216dc3942d0050021d85af7fa93d8877fbb91247
latihanajalah <latihanajalah@gmail.com>
2026-07-10 12:40:34 +0700

    Update start.sh to fix permissions and echo messages

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
+echo "Fixing permissions..."
+
+chown -R www-data:www-data /var/www/storage
+chown -R www-data:www-data /var/www/bootstrap/cache
+
+chmod -R 775 /var/www/storage
+chmod -R 775 /var/www/bootstrap/cache
+
 echo "Storage link..."
 php artisan storage:link || true
 
-echo "Migrating database..."
+echo "Migrating..."
 php artisan migrate --force || true
 
-echo "Optimizing Laravel..."
+echo "Optimizing..."
 php artisan optimize
 
 echo "Starting PHP-FPM..."
